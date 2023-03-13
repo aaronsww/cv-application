60e699f51b144ff4f2115f3ea589778ebdf60746
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-03-13 16:29:44 +0530

    style: colors and icons

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -36,7 +36,7 @@ function Display() {
           <div className="email">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="15"
+              width="15 "
               height="15"
               viewBox="0 0 24 24"
             >
